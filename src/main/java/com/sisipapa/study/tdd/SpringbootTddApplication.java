e1f9a3775fb07aaeeabfc2a75173edd2c65da1ec
ghkim <ghkim@enliple.com>
2021-08-20 12:58:08 +0900

    Initial commit

---
+++ src/main/java/com/sisipapa/study/tdd/SpringbootTddApplication.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sisipapa/study/tdd/SpringbootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
