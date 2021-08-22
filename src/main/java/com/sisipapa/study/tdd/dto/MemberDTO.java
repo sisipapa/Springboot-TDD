4585762a7d0afc6f2a45d18ef6e6ea4254ac0754
KKH <coolguy239@naver.com>
2021-08-22 09:20:39 +0900

    불필요한 import 제거

--- src/main/java/com/sisipapa/study/tdd/dto/MemberDTO.java
+++ src/main/java/com/sisipapa/study/tdd/dto/MemberDTO.java
@@ -1,13 +1,10 @@
 package com.sisipapa.study.tdd.dto;
 
-import com.sisipapa.study.tdd.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.Objects;
 
 @Data
 @Builder
