a35c7a84b2efbbfbf4b557cacbaebd5279fa40cc
KKH <coolguy239@naver.com>
2021-08-22 15:06:41 +0900

    RepositoryTest 내용수정

--- src/test/java/com/sisipapa/study/tdd/repository/MemberRepositoryTest.java
+++ src/test/java/com/sisipapa/study/tdd/repository/MemberRepositoryTest.java
@@ -1,13 +1,10 @@
 package com.sisipapa.study.tdd.repository;
 
-import com.sisipapa.study.tdd.dto.MemberDTO;
 import com.sisipapa.study.tdd.entity.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
