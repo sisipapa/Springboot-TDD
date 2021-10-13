49a066eb35ef946bfce6e9441a4f3a27938ffba3
ghkim <ghkim@enliple.com>
2021-10-13 15:50:55 +0900

    Java Stream peek!!

--- src/test/java/com/sisipapa/study/tdd/StreamBasic.java
+++ src/test/java/com/sisipapa/study/tdd/StreamBasic.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,10 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StreamBasic {
 
     @Test
+    void peek(){
+        int sum = IntStream.of(1,2,3,4,5)
+                .peek(System.out::println)
+                .sum();
+
+        assertEquals(15, sum);
+    }
+
+    @Test
+    void map(){
+        List<String> list = Arrays.asList("Java", "Scala", "Groovy", "Python", "Go", "Swift", "Java");
+
+        list.stream()
+                .map(str -> str.toLowerCase(Locale.ROOT))
+                .forEach(System.out::println);
+
+    }
+
+    @Test
     void filter(){
         List<String> list = Arrays.asList("Java", "Scala", "Groovy", "Python", "Go", "Swift", "Java");
 
-        System.out.println("### BEFORE");
         list.stream()
                 .filter(str -> str.startsWith("S"))
                 .collect(Collectors.toList())
