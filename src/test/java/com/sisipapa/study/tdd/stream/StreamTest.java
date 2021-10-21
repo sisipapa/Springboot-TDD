1f1c9d842fb1dae8451878b9d670befd592fd758
ghkim <ghkim@enliple.com>
2021-10-21 15:02:52 +0900

    모던 자바 stream 퀴즈

--- src/test/java/com/sisipapa/study/tdd/stream/StreamTest.java
+++ src/test/java/com/sisipapa/study/tdd/stream/StreamTest.java
@@ -71,10 +71,11 @@ public class StreamTest {
         // List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여
         // 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라. ex) ["Hello", "a", "Island", "b"] -> “HI”
         List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
-        WORDS.stream()
+        String result = WORDS.stream()
                 .filter(word -> word.length() >= 2)
-                .map(word -> word.toUpperCase())
-                .forEach(System.out::println);
+                .map(String::toUpperCase)
+                .reduce((a,b) -> a + " " + b).orElse("");
+        System.out.println(result);
     }
 }
 
